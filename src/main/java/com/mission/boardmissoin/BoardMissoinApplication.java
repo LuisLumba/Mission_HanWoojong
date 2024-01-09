e8b54ce4574071781059f69582cb10fdafb48c39
Woojong Han <141889922+LuisLumba@users.noreply.github.com>
2024-01-09 21:17:09 +0900

    리드미까지 깃허브에 제대로 올리기
    그동안 마크다운만 올리다가 인텔리제이?를 자바,스프링부트?를 올리는법을 몰라서 이것저것 시도해보다가 심플하고 간단하게 올리는방법 찾음
    그냥 업로드파일에 드레그하는게 잉네

---
+++ src/main/java/com/mission/boardmissoin/BoardMissoinApplication.java
@@ -0,0 +1,13 @@
+package com.mission.boardmissoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardMissoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardMissoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mission/boardmissoin/BoardMissoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mission.boardmissoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardMissoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
